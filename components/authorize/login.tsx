dafc8737131aa262a867a8ccb2e0b20fce9dbfa5
MaximChoy <m.choy@gosu.kz>
2023-05-04 17:09:57 +0600

    modified login modal window

--- components/authorize/login.tsx
+++ components/authorize/login.tsx
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
 import { Button, Form, Modal, Image, Col, Row } from "react-bootstrap";
-import PhoneInput from "react-phone-number-input/input";
 
 function Login(): JSX.Element {
   const [show, setShow] = useState(false);
   const [value, setValue] = useState<string | undefined>();
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const [phoneNumber, setPhoneNumber] = useState("");
 
+  const handlePhoneNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const cleanedPhoneNumber = e.target.value.replace(/\D/g, "");
+    const truncatedPhoneNumber = cleanedPhoneNumber.slice(0, 11);
+    const formattedPhoneNumber = truncatedPhoneNumber.replace(
+        /(\d{2})(\d{2})(\d{3})(\d{2})(\d{2})/,
+      "$1 $2 $3 $4 $5"
+      );
+    setPhoneNumber(formattedPhoneNumber);
+  };
   return (
     <>
       <Button className="btn_primary btn_login" size="sm" onClick={handleShow}>
@@ -19,19 +28,28 @@ function Login(): JSX.Element {
         <Image className="login_logo" src="login_logo.svg" />
 
         <Modal.Body>
-          <Modal.Title className="modal_heading">Добро пожаловать в Express Market!</Modal.Title>
-          <Modal.Title className="modal_subheading mt-1">Введите телефон для продолжения</Modal.Title>
+          <Modal.Title className="modal_heading">
+            Добро пожаловать в Express Market!
+          </Modal.Title>
+          <Modal.Title className="modal_subheading mt-1">
+            Введите телефон для продолжения
+          </Modal.Title>
           <Form>
-
             <Form.Group as={Row} className="phone_num_row my-4">
-              <Form.Label column className="phone_label" >
-                +7
+              <Form.Label column className="phone_label">
+                +9
               </Form.Label>
-              <Col className="phone_num" >
-                <Form.Control className="phone_num"  />
+              <Col className="phone_num">
+                <Form.Control
+                  type="tel"
+                  className="phone_num"
+                  value={phoneNumber}
+                  onChange={handlePhoneNumberChange}
+                  maxLength={14}
+                />
               </Col>
             </Form.Group>
-            <Button className="sms_btn" >Получить код по СМС</Button>
+            <Button className="sms_btn">Получить код по СМС</Button>
             <Button className="modal_login mt-3">Войти по почте</Button>
           </Form>
         </Modal.Body>
