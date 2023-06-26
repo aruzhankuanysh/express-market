4a3ea34da51922bbdb9388bb1c6ea45e87acdd16
artem.drozdov <a.drozdov@gosu.kz>
2023-06-26 18:43:02 +0600

    fix test cors

--- specs/axios.ts
+++ specs/axios.ts
@@ -3,7 +3,9 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: "https://test.4dev.kz/GOOD_FOOD/hs/GoodFood1C/",
     headers: {
-        "content-type":"application/json"
+        "content-type":"application/json",
+        "Accept":"application/json",
+        "mode": 'no-cors'
     }
 })
 
